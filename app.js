9c9ad209620e9e77528f2cd2c70ba4777cfeb286
anthonybillie <anthonymbillie@gmail.com>
2022-06-20 08:43:47 -0400

    working api

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/29485?unitGroup=us&include=days%2Ccurrent%2Chours%2Calerts%2Cevents&key=B8Q4A6P79H9BXDVVQTAWSLFFS&contentType=json", {
-  "method": "GET",
-  "headers": {
-  }
-  })
-.then(response => {
-  console.log(response.url);
-})
-.catch(err => {
-  console.error(err);
-});
 
+
+async function weatherAPI(){
+  const response = fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/29485?unitGroup=us&include=days%2Ccurrent%2Chours%2Calerts%2Cevents&key=B8Q4A6P79H9BXDVVQTAWSLFFS&contentType=json", {
+  "method": "GET",
+  })
+const data = (await response).json()
+return data
+}
+ weatherAPI().then(data =>{
+   console.log(data)
+ })
 
 
 //key=B8Q4A6P79H9BXDVVQTAWSLFFS
